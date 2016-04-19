893ae881e5c3f9c154abe820df0d966de97d3c5b
modifier <the.modifikator@gmail.com>
2016-04-19 22:38:52 +0300

    Change resource for parser

--- TextEditor/Controller/TextController.cs
+++ TextEditor/Controller/TextController.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Portable.Parser;
+using Portable.Parser.Grammar;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using TextEditor.Logic;
@@ -25,11 +24,33 @@ namespace TextEditor.Controller
 
         private AbstractCommand undoed;
 
+        private IParserFabric factory;
+
+        private string grammar = @"[OmitPattern(""[\s]*"")]
+[RootRule(expr)]
+SimpleArithmetics {
+
+    productOp: '*' | '/';
+    sumOp: '+' | '-';
+
+    [RewriteRecursion]
+    /*[ExpandRecursion]*/
+    #expr: {
+        |sum: expr sumOp expr;
+        |product: expr productOp expr;
+        |[right]power: expr '^' expr;
+        |#braces: '(' expr ')';
+        |num: ""[0-9]+"";
+    };
+}";
+
         public TextController(TextEditorRenderer renderer)
         {
             this.renderer = renderer;
             text = new Text(cursor);
             cursor.setText(text);
+
+            factory = getParserFactory(grammar);
         }
 
         public void keyPress(Key key)
@@ -426,6 +447,14 @@ namespace TextEditor.Controller
 
             renderer.DisplayText(transformText(text.text));
             displayCursor();
+        }
+
+        private IParserFabric getParserFactory(string grammar)
+        {
+            var grammarResult = DefinitionGrammar.Parse(grammar, false);
+            var rules = grammarResult.Rules.Cast<RuleSet>().ToArray();
+
+            return Parsers.CreateFabric(rules.First().Name, rules);
         }
     }
 }
